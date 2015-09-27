8a1a261de0493718160c3999536d4190773e8ff2
Kishan Gupta <kishan@kishangupta.com>
2015-09-27 12:03:50 +0530

    Reorganize assets
    Use LESS, reformat gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
 var webpack = require('webpack-stream');
+var less = require('gulp-less');
+var watchLess = require('gulp-watch-less');
 
 gulp.task('vendor-css', function() {
     return gulp.src([
@@ -33,14 +35,27 @@ gulp.task('vendor-js', function() {
 
 gulp.task('webpack', function() {
     return gulp.src('./assets/js/app.js')
-    .pipe(webpack({output: {filename: 'app-bundle.js'}}))
-    .pipe(gulp.dest('./js/'));
+        .pipe(webpack({output: {filename: 'app-bundle.js'}}))
+        .pipe(gulp.dest('./js'));
 });
 
-gulp.task('watch', function() {
+gulp.task('webpack-watch', function() {
     return gulp.src('./assets/js/app.js')
-    .pipe(webpack({watch: true, output: {filename: 'app-bundle.js'}}))
-    .pipe(gulp.dest('./js/'));
+        .pipe(webpack({watch: true, output: {filename: 'app-bundle.js'}}))
+        .pipe(gulp.dest('./js'));
 });
 
-gulp.task('default', ['vendor-css', 'vendor-js', 'webpack']);
+gulp.task('less', function() {
+    return gulp.src('./assets/less/app.less')
+        .pipe(less())
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('less-watch', function() {
+    return watchLess('./assets/less/app.less')
+        .pipe(less())
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', ['vendor-css', 'vendor-js', 'webpack', 'less']);
+gulp.task('watch', ['webpack-watch', 'less-watch']);
